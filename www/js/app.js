2fd2e48080c2d1b2b5813ccf5a7d60b582b875ad
Tomas Witek <tomas@witek.cz>
2015-06-08 15:24:40 +0200

    Basic login views

--- www/js/app.js
+++ www/js/app.js
@@ -27,18 +27,26 @@ angular.module('hubapp', [
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
-  $stateProvider.state('home', {
-    url: '/',
-    templateUrl: 'views/home.html'
-  });
+  $stateProvider
 
-  $stateProvider.state('products', {
+  .state('home', {
+    url: '/home',
+    templateUrl: 'views/home.html'
+  })
+
+  .state('login', {
+    url: '/',
+    templateUrl: 'views/login.html',
+    controller: 'LoginController'
+  })
+
+  .state('products', {
     url: '/products',
     templateUrl: 'views/products.html',
     controller: 'ProductsController'
-  });
+  })
 
-  $stateProvider.state('purchases', {
+  .state('purchases', {
     url: '/purchases',
     templateUrl: 'views/purchases.html',
     controller: 'PurchasesController'
